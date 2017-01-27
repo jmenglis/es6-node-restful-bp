d4f4048daa022a7bd095a5cafa3127b7260d9a9e
Josh English <josh@joshenglish.com>
2017-01-27 16:57:04 -0600

    All Tests passing, time for CI

--- tests/index-spec.js
+++ tests/index-spec.js
@@ -1,17 +1,25 @@
-import supertest from 'supertest'
+import request from 'supertest'
 let should    = require('chai').should(),
-    expect    = require('chai').expect(),
-    api       = supertest('http://localhost:4008');
+    expect    = require('chai').expect();
 
 describe('Server?', () => {
   describe('Is the API responding to commands?', () => {
+    let server;
+    beforeEach(() => {
+      server = require('../app/index');
+    });
+    afterEach(() => {
+      server.close()
+    });
     it('should return a 200 response', (done) => {
-      api.get('/')
+      request(server)
+        .get('/')
         .expect(200, done);
     });
 
     it('should return 404 for everything else', (done) => {
-      api.get('/something-else')
+      request(server)
+        .get('/foo/bar')
         .expect(404, done);
     });
 
