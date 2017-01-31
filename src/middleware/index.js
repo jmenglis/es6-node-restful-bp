1ef1261620cc0864e2b94659d820c1ea90c4c8a8
Josh English <josh@joshenglish.com>
2017-01-31 10:59:50 -0600

    Adding middleware file

---
+++ src/middleware/index.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+export default ({ config, db }) => {
+  let routes = Router();
+
+  /**
+   * Add middleware routes here
+   * Anything that needs to be used as a stack
+   * Can be used consistently
+   */
+
+  return routes;
+}
+
+
